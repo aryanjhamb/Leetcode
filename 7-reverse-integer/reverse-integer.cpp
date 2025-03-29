f4ac6dae8362fae34236a4335300fabb8f87884b
aryanjhamb <aryanjhamb.it26@gmail.com>
2025-03-29 09:02:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (54.34%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long t = 0;
+          while(x!=0)
+      {
+      
+        
+        t=t*10 + x%10;
+        x=x/10; 
+        
+        } 
+          if(t>INT_MAX || t<INT_MIN)
+        return 0;
+        return t;
+    }
+        
+    
+};
