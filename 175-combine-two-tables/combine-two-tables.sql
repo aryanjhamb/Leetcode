1a526563330e3f580a495189e950db60b0786950
aryanjhamb <aryanjhamb.it26@gmail.com>
2025-04-22 19:33:30 +0530

    Time: 410 ms (80.50%) | Memory: 0B (100.00%) - LeetSync

---
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+select p.lastName, p.firstName, a.city, a.state 
+from person p 
+left join
+address a 
+
+on p.personId = a.personId;
